4576b801c22ee7e9f479b4d13556dae483721cf2
Heri Agape <agape@live.fr>
2018-09-02 22:02:20 +0300

    added default launch

--- app.py
+++ app.py
@@ -106,5 +106,8 @@ def versions():
     return jsonify(data)
 
 
-if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=8080)
+# if __name__ == "__main__":
+#     app.run(debug=False, host='0.0.0.0', port=8080)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
